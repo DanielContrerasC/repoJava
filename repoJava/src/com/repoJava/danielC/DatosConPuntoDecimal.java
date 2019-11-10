ff4f8bb97670b3b122759ad13610aced8c641707
AdonisDanCC <daniel.trabajo.desarrollo@gmial.com>
2019-11-09 23:50:30 -0600

    Agregando java.class Tipos de datos con punto decimal

---
+++ repoJava/src/com/repoJava/danielC/DatosConPuntoDecimal.java
@@ -0,0 +1,22 @@
+package com.repoJava.danielC;
+
+public class DatosConPuntoDecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		byte edad =127;
+		short year =-32768;
+		int in_user = 1001;
+		long id_twitter = 123456789123456789L;
+		
+		//Punto Flotante 
+		//Rango de 1.40129846432481707e-45 o 4bites
+		//Rango de Double 4.94065645841246544e-324d
+
+			float diametro= 34.25f;
+			double precio = 1234567.12345678;
+			
+	}
+
+}
