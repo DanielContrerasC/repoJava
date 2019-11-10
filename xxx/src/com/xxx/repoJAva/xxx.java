25bbb481173eb60b5df885a69a5d5e9b1e749e6b
AdonisDanCC <daniel.trabajo.desarrollo@gmial.com>
2019-11-09 23:23:57 -0600

    Creando el primer  hola mundo del repoJava

---
+++ xxx/src/com/xxx/repoJAva/xxx.java
@@ -0,0 +1,10 @@
+package com.xxx.repoJAva;
+
+public class xxx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hola mundo otra vez");
+	}
+
+}
