9cae3c7e16edbeaa9dbbf8262fd5f5dda3a808c4
AdonisDanCC <daniel.trabajo.desarrollo@gmial.com>
2019-11-13 22:52:17 -0600

    agregando los Arrays

---
+++ Arrays/src/com/Arrays/Adonis/Arreglos.java
@@ -0,0 +1,38 @@
+package com.Arrays.Adonis;
+
+public class Arreglos {
+	/*Los arreglos se pueden definir como OBJETOS en los que podemos
+	 * guardar mas de una variable.
+	 * 
+	 * Declaracion de Arreglos  : Tipo de Dato [] nombreVariable;
+	 * 							  Tipo de Dato nombreVariable[];
+	 * 
+	 * Definir el tamaño de los arreglos:
+	 * 
+	 * nombreVariable =new TipoDeDato[capacidad]
+	 * 
+	 * La cantidad de dimeciones va a depender de la cantidad de corchetes que se pongas 
+	 * Y la cantidad de elementos se coloca dentro de los corchetes[] [] [] N-Dimenciones
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Arrays Ejemplo
+		
+		//int [] arregloInt;
+		//Arreglo de una dimencion [] , y declarando la capacidad del arreglo[n-elementos]
+		int [] arregloInt = new int[3];
+		double arregloDouble []; 
+		
+		//Arreglo de dos dimenciones
+		
+		int [] [] array2D=new int [][];
+		
+		
+			
+		}
+
+	}
+
+}
