3fb065c95bc363a864aba0ffbf1f3878e8d37fe4
DanielContreras <adoniscas@hotmail.com>
2019-11-20 14:27:44 -0600

    iniciando prioyecto final

--- AmazonViewer/src/com/AdonisDCC/amazonviewer/Main.java
+++ AmazonViewer/src/com/AdonisDCC/amazonviewer/Main.java
@@ -1,4 +1,14 @@
 package com.AdonisDCC.amazonviewer;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("BIENVENIDOS A AMAZON VIEW");
+        System.err.println("");
+        System.out.println("Selecciona el menu de la opcion deseada");
+        System.out.println("1. Movies ");
+        System.out.println("2. Series ");
+        System.out.println("3. Books  ");
+        System.out.println("4. Magazines");
+
+    }
 }
