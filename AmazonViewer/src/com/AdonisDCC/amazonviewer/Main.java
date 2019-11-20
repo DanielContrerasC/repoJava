fcdf12b1a844274fe6ef06031e59315c440cd44b
DanielContreras <adoniscas@hotmail.com>
2019-11-20 15:16:46 -0600

    Agregando sentencia do white

--- AmazonViewer/src/com/AdonisDCC/amazonviewer/Main.java
+++ AmazonViewer/src/com/AdonisDCC/amazonviewer/Main.java
@@ -2,13 +2,20 @@ package com.AdonisDCC.amazonviewer;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("BIENVENIDOS A AMAZON VIEW");
-        System.err.println("");
-        System.out.println("Selecciona el menu de la opcion deseada");
-        System.out.println("1. Movies ");
-        System.out.println("2. Series ");
-        System.out.println("3. Books  ");
-        System.out.println("4. Magazines");
+
+        int exit = 0;
+
+        do{
+            System.out.println("BIENVENIDOS A AMAZON VIEW");
+            System.err.println("");
+            System.out.println("Selecciona el menu de la opcion deseada");
+            System.out.println("1. Movies ");
+            System.out.println("2. Series ");
+            System.out.println("3. Books  ");
+            System.out.println("4. Magazines");
+            System.out.println("0. Exit ");
+
+        }while (exit !=0);
 
     }
 }
