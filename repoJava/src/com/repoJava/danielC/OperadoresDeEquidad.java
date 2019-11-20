7ab19523f3201b5a74b74d13c33fbb54ece006fc
AdonisDanCC <daniel.trabajo.desarrollo@gmial.com>
2019-11-20 01:28:03 -0600

    Agregando la clase de operadores relacionales

---
+++ repoJava/src/com/repoJava/danielC/OperadoresDeEquidad.java
@@ -0,0 +1,24 @@
+package com.repoJava.danielC;
+
+public class OperadoresDeEquidad {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*los operadores de equidad son los siguientes
+		
+		operador     Nombre       Ejemplo
+		
+		 ==			igualdad         a==b
+		 
+		 !=			desigualdad      a!=b*/
+		
+		int x =5;
+		int y=4;
+		
+		System.out.println( x ==y);
+
+	}
+
+}
---
+++ repoJava/src/com/repoJava/danielC/OperadoresRelacionales.java
@@ -0,0 +1,35 @@
+package com.repoJava.danielC;
+
+public class OperadoresRelacionales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Operadores Relacionales
+		 * 
+		 * 	Operador	 Nombre 	Ejemplo
+		 * 		<		Menor que		a<b
+		 * 		>		Mayor que		a>b
+		 * 		<=		Menor o igual   a<=b
+		 * 					que
+		 * 		>=		Mayor o igual	a>=b
+		 * 					que	
+		 * 
+		 */
+		
+		int a =5;
+		int b =7;
+		int c =5;
+		
+		System.out.println(" Numeros a comparar a = 5 b = 7 c=5");
+		System.out.println("Operador relacional mayor que b>a: "+ (b > a) );
+		System.out.println("Operador relacional menor que a<b: " +(a<b));
+		System.out.println("Operador relacional Menor o igual que a>=c: "+(a>=c) );
+		System.out.println("Operador relacional Mayor o igual que b<=c: "+(b<=c));
+		
+		
+		
+	}
+
+}
