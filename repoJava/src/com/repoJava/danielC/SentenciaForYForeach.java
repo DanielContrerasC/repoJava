673f91f1bfe777717ff354bc5d537b378ad739e9
DanielContreras <adoniscas@hotmail.com>
2019-11-22 17:37:52 -0600

    Iniciando sentencia For Foreach y explicacion de uso.

--- repoJava/src/com/repoJava/danielC/SentenciaForYForeach.java
+++ repoJava/src/com/repoJava/danielC/SentenciaForYForeach.java
@@ -2,6 +2,36 @@ package com.repoJava.danielC;
 
 public class SentenciaForYForeach {
     public static void main(String[] args) {
+
+        /*
+        Con la sentencia for se puede manejar el indice y generar mas contenido
+        en la sentencia segun sea los requerimientos del desarrollo que se esta
+        ejecutado
+         */
+
+        for (int i = 0; i <=5 ; i++) {
+
+            System.out.println("i es menor que 5 " +i);
+        }
+
+        System.out.println();
+        int [] numeros = new int[5];
+        for (int c = 0; c<5; c++){
+            numeros[c]=c;
+            System.out.println("Numeros ["+c+"]: " +numeros[c]);
+        }
+        /*
+        el for each es una forma mas rapida de poder ejecutar el bucle.
+        aunque no c tiene acceso a el indice
+         */
+
+
+        for (int j:numeros ) {
+            System.out.println(j);
+
+        }
+
+        
         
     }
 }
