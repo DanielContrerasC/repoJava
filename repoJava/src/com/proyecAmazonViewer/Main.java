7a84081b62ec23301b5fcae1da98d647e7eb4362
DanielContreras <adoniscas@hotmail.com>
2019-11-24 21:17:44 -0600

    Agregando metodo a main

--- repoJava/src/com/proyecAmazonViewer/Main.java
+++ repoJava/src/com/proyecAmazonViewer/Main.java
@@ -1,8 +1,71 @@
 package com.proyecAmazonViewer;
 
+import com.proyecAmazonViewer.model.Movie;
+
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+
+        Movie movie = new Movie();
+        movie.title  = "COCO";
+        movie.duration = 120;
+
+
+        int exit =0;
+        do{
+            System.out.println("BIENVENIDOS A AMAZON VIEW");
+            System.err.println("");
+            System.out.println("Selecciona el menu de la opcion deseada");
+            System.out.println("1. Movies ");
+            System.out.println("2. Series ");
+            System.out.println("3. Books  ");
+            System.out.println("4. Magazines");
+            System.out.println("5. Make Report");
+            System.out.println("6. Make report Today");//mostrara ordenes por fecha
+            System.out.println("0. Exit ");
+            System.out.println();
+
+            Scanner sc =new Scanner(System.in);
+            int response = Integer.valueOf(sc.nextLine());
+
+            switch (response){
+                case 0:
+                    //salir
+                    exit =0;
+                    break;
+                case 1:
+                    showMovies();
+                break;
+                case 2:
+                    showSeries();
+                    break;
+                case 3:
+                    showBooks();
+                    break;
+                case 4:
+                    showMagazines();
+                    break;
+                case 5:
+                    //   makeReport();
+                    break;
+                case 6:
+                    //   makeReport(new Date())
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("....!!Selecciona Una Opcion¡¡....");
+                    System.out.println();
+                    break;
+
+            }
+
+
+        }while (exit != 0);
     }
+
 }
--- repoJava/src/com/proyecAmazonViewer/model/Movie.java
+++ repoJava/src/com/proyecAmazonViewer/model/Movie.java
@@ -1,14 +1,15 @@
 package com.proyecAmazonViewer.model;
 
 public class Movie {
-    int id;
-    String title;
-    String genre;
-    String creator;
-    int duration;
-    short year;
-    boolean viewer;
-    int timeViewer;
+
+     public int id;
+     public String title;
+    public String genre;
+    public  String creator;
+    public int duration;
+    public short year;
+    public boolean viewer;
+    public int timeViewer;
 
 
 }
