ef5ad84a81b15d042236b69ad2c4b87df37e5a15
DanielContreras <adoniscas@hotmail.com>
2019-11-29 15:45:00 -0600

    que es herencia, que  es super, y aplicacion del this, poliformismo

--- repoJava/src/com/proyecAmazonViewer/model/Movie.java
+++ repoJava/src/com/proyecAmazonViewer/model/Movie.java
@@ -1,93 +1,24 @@
 package com.proyecAmazonViewer.model;
 
-public class Movie {
+public class Movie extends Film {
 
     private int id;
-    private String title;
-    private String genre;
-    private String creator;
-    private int duration;
-    private short year;
-    private boolean viewer;
     private int timeViewer;
 
-    public Movie(){
-        super();
-        this.title=title;
-        this.genre =genre;
-        this.creator=creator;
-        this.duration=duration;
-        this.year =year;
-
-    }
-    public Movie(String title, String genre, short year){
-        super();
-        this.title =title;
-        this.genre=genre;
-        this.year=year;
-
+    public Movie(String title, String genre, String creator, int duration, short year) {
+        super(title, genre, creator, duration);
+       setYear(year);
     }
 
     public void showData(){
-        System.out.println("Title: " +title);
-        System.out.println("Genre: " + genre);
-        System.out.println("year: " +year);
-    }
-    public String getTitle(){
-        return title;
-    }
-    public void setTitle(String title){
-        this.title=title;
+        //System.out.println("Title: " +title);
+        //System.out.println("Genre: " + genre);
+        //System.out.println("year: " +year);
     }
 
     public int getId() {
         return id;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getCreator() {
-        return creator;
-    }
-
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public short getYear() {
-        return year;
-    }
-
-    public void setYear(short year) {
-        this.year = year;
-    }
-
-    public boolean isViewer() {
-        return viewer;
-    }
-
-    public void setViewer(boolean viewer) {
-        this.viewer = viewer;
-    }
-
     public int getTimeViewer() {
         return timeViewer;
     }
--- repoJava/src/com/proyecAmazonViewer/model/Series.java
+++ repoJava/src/com/proyecAmazonViewer/model/Series.java
@@ -1,89 +1,19 @@
 package com.proyecAmazonViewer.model;
 
-public class Series {
+public class Series extends Film {
 
     private int id;
-    private String title;
-    private String genre;
-    private String creator;
-    private int duration;
-    private short year;
-    private boolean viewer;
-    private int timeViewer;
+    private int sessionQuantity;
+    private Chapter[] chapters;
 
-    int sessionQuantity;
-    //chapters[]
+    public Series(String title, String genre, String creator, int duration, int sessionQuantity) {
+        super(title, genre, creator, duration);
+        this.sessionQuantity=sessionQuantity;
 
-
-    public Series(String title, String genre, int duration) {
-        super();
-        this.title = title;
-        this.genre = genre;
-        this.duration = duration;
     }
 
-    public int getId() {
+    public int getId(){
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getCreator() {
-        return creator;
-    }
-
-    public void setCreator(String creator) {
-        this.creator = creator;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public short getYear() {
-        return year;
-    }
-
-    public void setYear(short year) {
-        this.year = year;
-    }
-
-    public boolean isViewer() {
-        return viewer;
-    }
-
-    public void setViewer(boolean viewer) {
-        this.viewer = viewer;
-    }
-
-    public int getTimeViewer() {
-        return timeViewer;
-    }
-
-    public void setTimeViewer(int timeViewer) {
-        this.timeViewer = timeViewer;
     }
 
     public int getSessionQuantity() {
@@ -92,5 +22,13 @@ public class Series {
 
     public void setSessionQuantity(int sessionQuantity) {
         this.sessionQuantity = sessionQuantity;
+    }
+
+    public Chapter[] getChapters() {
+        return chapters;
+    }
+
+    public void setChapters(Chapter[] chapters) {
+        this.chapters = chapters;
     }
 }
